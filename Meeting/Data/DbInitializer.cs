358bce59593673ecc0505bc502aaa77d22dd1c28
Alves <andre.alves02@sap.com>
2018-06-13 17:33:56 +0100

    Created Meeting Microservice

---
+++ Meeting/Data/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Meeting.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Meeting.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(MeetingContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            /*if (context.Teachers.Any()) //DB has been seeded
+                return;*/
+
+            
+        }
+    }
+}
---
+++ Meeting/Data/MeetingContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meeting.Data
+{
+    public class MeetingContext : DbContext
+    {
+        public MeetingContext(DbContextOptions<MeetingContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
