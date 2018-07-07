2e046673244dcc49a56fd3f884aa68455a3faad0
André Alves <andre.pi.alves@gmail.com>
2018-07-07 15:10:43 +0100

    Create RabbitMQ code

---
+++ EventBus_/Abstractions/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace EventBus_.Abstractions
+{
+    public interface IDynamicIntegrationEventHandler
+    {
+        Task Handle(dynamic eventData);
+    }
+}
---
+++ EventBus_/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using EventBus_.Events;
+using System.Threading.Tasks;
+
+namespace EventBus_.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
